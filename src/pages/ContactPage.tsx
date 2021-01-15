b26e05815bf93fd01ac1c7dabdfdcf6900720626
moret5u <72914371+MORET5U@users.noreply.github.com>
2021-01-15 13:10:58 +0400

    no discord only telegram and email

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTelegramPlane, faDiscord } from "@fortawesome/free-brands-svg-icons";
+import { faTelegramPlane } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { useTranslation } from "react-i18next";
 import MD from "react-markdown";
