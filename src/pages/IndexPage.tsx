c9105c5d51dc02de3d8ba3d5d598946cee8bbc79
moret5u <72914371+MORET5U@users.noreply.github.com>
2021-01-15 22:54:39 +0400

    switched to react-image in favor of fallbacks

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import MD from "react-markdown";
+import { OCTOCAT } from "../data";
+import { Img } from "react-image";
 
 const IndexPage = () => {
   const { t } = useTranslation();
@@ -8,7 +10,12 @@ const IndexPage = () => {
   return (
     <>
       <section className="content">
-        <img src="https://github.com/MORET5U.png" className="avatar" alt="avatar" />
+        <Img
+          className="avatar"
+          src="https://github.com/MORET5U.png"
+          loader={<img src={OCTOCAT} className="avatar" />}
+          unloader={<img src={OCTOCAT} className="avatar" />}
+        />
         <h1 className="title-1">moretsu</h1>
 
         <MD source={t("index_paragraph_1")!} />
